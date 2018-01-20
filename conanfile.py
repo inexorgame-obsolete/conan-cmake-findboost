6c4a8c51efb9a2fc1f46d1c70ef816fac83834b9
Croydon <cr0ydon@outlook.com>
2018-01-20 14:48:53 +0100

    Bump version number

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class CMakeFindBoost(ConanFile):
     name = "cmake-findboost"
-    version = "0.2.0"
+    version = "0.2.1"
     license = "<Put the package license here>"
     url = "<Package recipe repository url here, for issues about the package>"
     description = "Supports CMake's findBoost methode for the bincrafters'Boost packages."
